2ce7a85aa94c8db8547bc214e8b94fe6d7db7dd7
Ali <A_1377ranjbar@yahoo.com>
2019-04-01 19:13:11 +0430

    Updates

---
+++ precache-manifest.c6f59d8efb86ebee02af64b287673b50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9b8843d0d5da79e0551",
+    "url": "/contact-manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "d9b8843d0d5da79e0551",
+    "url": "/contact-manager/static/js/main.188d9602.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  },
+  {
+    "revision": "abfde09737fa2c354350",
+    "url": "/contact-manager/static/css/2.c2f917c1.chunk.css"
+  },
+  {
+    "revision": "abfde09737fa2c354350",
+    "url": "/contact-manager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "f1b5d3e5659c156216fc531d2bd33d49",
+    "url": "/contact-manager/index.html"
+  }
+];
